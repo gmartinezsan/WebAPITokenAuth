4bf06cf527336b13cae71d351d1756504daa4604
Gab Martinez <gmartinezsan@gmail.com>
2018-04-26 16:56:13 -0500

    Simple example about using bearer auth with asp.net core

---
+++ JWTExample/Controllers/TokenController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTExample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+
+    private IConfiguration _config;
+
+    public TokenController(IConfiguration config)
+    {
+      _config = config;
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public IActionResult CreateToken([FromBody]LoginModel login)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest();
+      }
+
+      IActionResult response = Unauthorized();
+
+      var user = Authenticate(login);
+
+      if (user != null)
+      {
+        var tokenString = BuildToken(user);
+        response = Ok(new { token = tokenString });
+      }
+
+      return response;
+    }
+
+    private string BuildToken(UserModel user)
+    {
+
+      var claims = new[] {
+        new Claim(JwtRegisteredClaimNames.Sub, user.Name),
+        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+        new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd")),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+      var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+        _config["Jwt:Issuer"],
+        claims,
+        expires: DateTime.Now.AddMinutes(30),
+        signingCredentials: creds);
+
+      return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserModel Authenticate(LoginModel login)
+    {
+      UserModel user = null;
+
+      if (login.Username == "juan" && login.Password == "MyP4ssw0rd!")
+      {
+        user = new UserModel { Name = "Juan Perez", Email = "juan.perez@somemail.com" };
+      }
+
+      if (login.Username == "marie" && login.Password == "MyP4ssw0rd!")
+      {
+        user = new UserModel { Name = "Marie Perez", Email = "marie.perez@somemail.com", Birthdate=new DateTime(2007, 1, 1) };
+      }
+
+      return user;
+    }
+
+    public class LoginModel
+    {
+      public string Username { get; set; }
+      public string Password { get; set; }
+    }
+
+    private class UserModel
+    {
+      public string Name { get; set; }
+      public string Email { get; set; }
+      public DateTime Birthdate { get; set; }
+    }
+  }
+}
